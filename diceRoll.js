58398964b8a85e8b8615618f0564022c9f668985
Kathy Gee <mzchick@gmail.com>
2021-02-25 12:12:26 -0500

    second commit

--- diceRoll.js
+++ diceRoll.js
@@ -1,8 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const input = Number(document.querySelector('input')).value
+    const input = document.querySelector('#dice-input')
     const diceHistory = document.querySelector('ul')
+    const diceSum = document.querySelector('#sum-para')
+    const button = document.querySelector('#dice-roll')
+    const diceValue = {
+        "&#9856" : 1 ,
+        "&#9857" : 2 ,
+        "&#9858" : 3 ,
+        "&#9859" : 4 ,
+        "&#9860" : 5 ,
+        "&#9861" : 6 ,
+    }
+    
+    button.addEventListener('click', () => {
+        event.preventDefault()
+    })
+    }
+
+    
+
+    document.querySelector('form').addEventListener('submit', (event) => {
+        event.preventDefault()
+        for (let i = 0, i > 0; i++) {
+            
+        }
+
+
+    })
 
     const diceRolled = document.createElement('li)')
     diceRolled.textContent = input
     diceHistory.appendChild(diceRolled)
-})
+}
