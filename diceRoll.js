4868b4bdd88d387f682b4358e740f129a2b69ab5
Kathy Gee <mzchick@gmail.com>
2021-02-25 12:36:07 -0500

    third commit

--- diceRoll.js
+++ diceRoll.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const input = document.querySelector('#dice-input')
     const diceHistory = document.querySelector('ul')
     const diceSum = document.querySelector('#sum-para')
-    const button = document.querySelector('#dice-roll')
+    const button = document.querySelector('#dice-roll-button')
+    const object = {}
     const diceValue = {
         "&#9856" : 1 ,
         "&#9857" : 2 ,
@@ -11,24 +12,30 @@ document.addEventListener("DOMContentLoaded", () => {
         "&#9860" : 5 ,
         "&#9861" : 6 ,
     }
-    
-    button.addEventListener('click', () => {
-        event.preventDefault()
-    })
-    }
+    const object = {}
 
-    
-
-    document.querySelector('form').addEventListener('submit', (event) => {
-        event.preventDefault()
-        for (let i = 0, i > 0; i++) {
-            
-        }
-
-
-    })
-
-    const diceRolled = document.createElement('li)')
-    diceRolled.textContent = input
-    diceHistory.appendChild(diceRolled)
 }
+
+
+
+    
+//     button.addEventListener('click', () => {
+//         event.preventDefault()
+//     })
+//     }
+
+    
+
+//     document.querySelector('form').addEventListener('submit', (event) => {
+//         event.preventDefault()
+//         for (let i = 0, i > 0; i++) {
+            
+//         }
+
+
+//     })
+
+//     const diceRolled = document.createElement('li)')
+//     diceRolled.textContent = input
+//     diceHistory.appendChild(diceRolled)
+
