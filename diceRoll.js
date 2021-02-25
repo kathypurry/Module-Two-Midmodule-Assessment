39b77df90a387e8dcfb2f97b4dea9ac333ca3608
Kathy Gee <mzchick@gmail.com>
2021-02-25 15:11:25 -0500

    refactoring

--- diceRoll.js
+++ diceRoll.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentloaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     const diceValue = [
         "&#9856",
         "&#9857",
@@ -10,9 +10,10 @@ document.addEventListener("DOMContentloaded", () => {
 
     const diceInput = document.getElementById('dice-input')
     const diceOutput = document.getElementById('dice-para')
-    const sum = document.getElementById('sum-para')
+    let sumOutput = document.getElementById('sum-para')
     const button = document.getElementById('dice-roll-button')
-    const ul = document.getElementById('ul')
+    const ul = document.querySelector('ul')
+    
 
     button.addEventListener('click', () => {
         event.preventDefault()
@@ -20,35 +21,94 @@ document.addEventListener("DOMContentloaded", () => {
         let arr = []
         for (let i = 0; i < diceInput.value; i++) {
             arr[i] = diceValue[Math.floor(Math.random() * 6)]
+            if (arr[i] === "&#9856") {
+                sumOutput += 1
+            }else if (arr[i] === "&#9857") {
+                sumOutput += 2
+            } else if (arr[i] === "&#9858") {
+                sumOutput +=3
+            } else if (arr[i] === "&#9859") {
+                sumOutput += 4
+            } else if (arr[i] === "&#9860") {
+                sumOutput += 5
+            } else if (arr[i] === "&#9861") {
+                sumOutput += 6
+            }
+
         }
 
-        let sumOutput = 0
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] === "&9856") {
-                sum += 1
-            }else if (arr[i] === "&9857") {
-                sum += 2
-            } else if (arr[i] === "&9858") {
-                sum +=3
-            } else if (arr[i] === "&9859") {
-                sum += 4
-            } else if (arr[i] === "&9860") {
-                sum += 5
-            } else if (arr[i] === "&9861") {
-                sum += 6
-            }
+        // let sumOutput = 0
+        // for (let i = 0; i < arr.length; i++) {
+        //     if (arr[i] === "&#9856") {
+        //         sumOutput += 1
+        //     }else if (arr[i] === "&#9857") {
+        //         sumOutput += 2
+        //     } else if (arr[i] === "&#9858") {
+        //         sumOutput +=3
+        //     } else if (arr[i] === "&#9859") {
+        //         sumOutput += 4
+        //     } else if (arr[i] === "&#9860") {
+        //         sumOutput += 5
+        //     } else if (arr[i] === "&#9861") {
+        //         sumOutput += 6
+        //     }
             
-        }
+        // }
         let newRoll = document.createElement('li')
         newRoll.innerHTML = `${sumOutput} ${arr.join('')}`
         ul.appendChild(newRoll)
 
-        sum.innerHTML = `Sum = ${sumOutput}`
+        sumOutput.innerHTML = `Sum = ${sumOutput}`
         diceOutput.innerHTML = arr.join('')
 
 
     })
 })
+
+console.log('hello')
+
+// document.addEventListener("DOMContentLoaded", () => {
+
+//     const dice = {
+//         "1": "&#9856", 
+//         "2": "&#9857", 
+//         "3": "&#9858", 
+//         "4": "&#9859", 
+//         "5": "&#9860", 
+//         "6": "&#9861", 
+//     };
+
+//     let diceInput = document.getElementById('dice-input');
+//     const diceContainer = document.getElementById('dice-para');
+//     const sumNumber = document.getElementById('sum-para');
+//     const diceHistory = document.getElementById('previous-rolls');
+//     let firstRoll = true;
+
+//     const rollTheDie = () => (Math.floor(Math.random() * 6) + 1).toString();
+//     document.getElementById("roller").addEventListener("submit", (e) => {
+
+//         e.preventDefault();
+//         if (firstRoll){
+//             firstRoll = false;
+//         } else {
+
+//             let listTag = document.createElement("li");
+//             listTag.innerHTML = `${diceContainer.innerHTML}=${sumNumber.innerHTML}`;
+//             diceHistory.appendChild(listTag);
+//             diceContainer.innerHTML = "";
+//         };
+//         let diceNum = diceInput.value;
+//         diceInput.value = "";
+//         let diceSum = 0;
+//         for (let i = 0; i < diceNum; i++){
+//             let rollValue = rollTheDie();
+//             diceSum += parseInt(rollValue, 10);
+//             diceContainer.innerHTML += dice[rollValue];
+//         }
+//         sumNumber.innerHTML = diceSum;    
+//     });
+// });
+
 
 
 
